4248b42a1bc2dcb8fefe1abd6762769c435e83e0
PapyrTigris <papyrtigris@gmail.com>
2014-12-06 11:15:20 +0100

    OCL frissítése

--- app/src/main/java/hu/uniobuda/nik/torpedo/Tabla.java
+++ app/src/main/java/hu/uniobuda/nik/torpedo/Tabla.java
@@ -1,71 +1,79 @@
 package hu.uniobuda.nik.torpedo;
 
 import android.content.Context;
-import android.content.DialogInterface;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import android.content.Intent;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
+
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
-import android.widget.ImageView;
 
-import java.util.Objects;
 
 
 public class Tabla extends BaseAdapter {
 
     Context mContext;
-    public Integer[] alapkockak;
+    Button[] gombok ;
     int db;
     View.OnClickListener OCL;
 
     public Tabla(final Context mContext) {
         this.mContext = mContext;
         db = Solo_Multy_Activity.sor * Solo_Multy_Activity.oszlop;
-        alapkockak = new Integer[db];
-        for (int i = 0; i < db; i++) {
-            alapkockak[i] = R.drawable.alap;
-        }
+        gombok = new Button[Solo_Multy_Activity.oszlop*Solo_Multy_Activity.sor];
+        //alapkockak = new Integer[db];
+
+
+        //for (int i = 0; i < db; i++) {
+        //    alapkockak[i] = R.drawable.alap;
+        //}
         OCL = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                //Lőhetsz
                 int i = view.getId();
-                alapkockak[i] = R.drawable.viz;
+                //alapkockak[i] = R.drawable.viz;
+               // view.
+                gombok[i].setText("X");
+                gombok[i].setEnabled(false);
+
+                //mContext.startActivity(new Intent());
                 view.invalidate();
+
             }
         };
     }
 
     @Override
     public int getCount() {
-        return alapkockak.length;
+        return gombok.length;
     }
 
     @Override
     public Object getItem(int i) {
-        return alapkockak[i];
+        return gombok[i];
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return gombok[i].getId();
     }
 
 
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-
         Button B = new Button(mContext);
+
         B.setId(i);
-        B.setLayoutParams(new GridView.LayoutParams(70,70));
+        B.setLayoutParams(new GridView.LayoutParams(70, 70));
         B.setOnClickListener(OCL);
+      //  B.setTextAlignment(View.TEXT_ALIGNMENT_CENTER); Nem engedélyezett a 2.3 miatt
+        
+        gombok[i]= B;
         return B;
     }
 }
